fd012506f251b605460e0125325521cc34183c1f
rbatsenko <rbatsenko@gmail.com>
2018-10-19 11:57:57 +0200

    change to Google Maps

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
 class App extends Component {
-  loadMap = () => {
-    
+  mapStyles = {
+    width: '100%',
+    height: 'calc(100vh - 64px)'
   }
 
   componentDidMount() {
-    this.loadMap();
+    //this.loadMap();
   }
 
   render() {
@@ -18,10 +19,22 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <div id="map"></div>
+        <div id="google-map">
+          <Map
+            google={this.props.google}
+            zoom={12}
+            style={this.mapStyles}
+            initialCenter={{
+              lat: 50.0487156,
+              lng: 19.9328273
+            }}
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBlVCXYggvwxrrHWeEqePL1FN5oCiJ8czw'
+})(App);
