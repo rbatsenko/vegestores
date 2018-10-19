d9b30b79c4b22bca20e658db91595257019082e7
rbatsenko <rbatsenko@gmail.com>
2018-10-19 20:21:48 +0200

    search input

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,16 @@ import '../styles/App.css';
 import MapWithLocation from './Map';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <a href="/"><img src={logo} className="App-logo" alt="Warzywniak - Info" /></a>
+        <header className="App-header clearfix">
+          <a className="App-logo-link" href="/"><img src={logo} className="App-logo" alt="Warzywniak - Info" /></a>
+          <div className="App-search">
+            <h1>Znajdź warzywniak</h1>
+            <input className="App-search-field" id="search" name="search" placeholder="Wpisz adres..." />
+          </div>
         </header>
         <main>
           <MapWithLocation />
