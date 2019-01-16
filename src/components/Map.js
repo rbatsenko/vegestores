cefc56ad24bba67ba77e26f3a52d651e2244ef5d
Roman Batsenko <rbatsenko@gmail.com>
2019-01-16 09:32:15 +0100

    some more destructuring to Map Component

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,6 +17,147 @@ class MapWithLocation extends Component {
     height: 'calc(100vh - 64px)'
   }
 
+  snazzyMapsStyles = [
+    {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "hue": "#ffee00"
+          },
+          {
+              "saturation": "50"
+          }
+      ]
+    },
+    {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#d0e3b4"
+          }
+      ]
+    },
+    {
+      "featureType": "landscape.natural.terrain",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },
+    {
+      "featureType": "poi.medical",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#fbd3da"
+          }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#bde6ab"
+          }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffe15f"
+          }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#efd151"
+          }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "black"
+          }
+      ]
+    },
+    {
+      "featureType": "transit.station.airport",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#cfb2db"
+          }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#a2daf2"
+          }
+      ]
+    }
+  ];
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
@@ -35,149 +176,8 @@ class MapWithLocation extends Component {
   }
 
   render() {
-    const { loading, userLocation } = this.state;
-    const { google } = this.props;
-    const snazzyMapsStyles =
-    [
-      {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#ffee00"
-            },
-            {
-                "saturation": "50"
-            }
-        ]
-      },
-      {
-        "featureType": "landscape.natural",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#d0e3b4"
-            }
-        ]
-      },
-      {
-        "featureType": "landscape.natural.terrain",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-      },
-      {
-        "featureType": "poi.business",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-      },
-      {
-        "featureType": "poi.medical",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#fbd3da"
-            }
-        ]
-      },
-      {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#bde6ab"
-            }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffe15f"
-            }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#efd151"
-            }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-      },
-      {
-        "featureType": "road.local",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "black"
-            }
-        ]
-      },
-      {
-        "featureType": "transit.station.airport",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#cfb2db"
-            }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#a2daf2"
-            }
-        ]
-      }
-    ];
+    const { loading, userLocation, zoom } = this.state;
+    const { google, address } = this.props;
 
     if (loading) {
       return (
@@ -190,17 +190,17 @@ class MapWithLocation extends Component {
     return (
       <div id="google-map">
         <Map
-          google={google}
-          zoom={typeof this.props.address.zoom !== undefined ? this.props.address.zoom : this.state.zoom}
-          style={this.mapStyles}
-          center={this.props.address.latLng}
-          initialCenter={userLocation}
-          styles={snazzyMapsStyles}
+          google={ google }
+          zoom={ typeof address.zoom !== undefined ? address.zoom : zoom }
+          style={ this.mapStyles }
+          center={ address.latLng }
+          initialCenter={ userLocation }
+          styles={ this.snazzyMapsStyles }
         >
           <Marker
-            title={'Your position'}
-            name={'Your position'}
-            position={userLocation}
+            title={ 'Your position' }
+            name={ 'Your position' }
+            position={ userLocation }
             icon={{
               url: '/user-pin.png',
             }}
